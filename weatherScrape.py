2da2d1705975d3c0a87628b54d27d4456e9172bf
Chris McKissack <75745188+ChrisMcK1@users.noreply.github.com>
2021-01-26 20:52:21 -0600

    Update weatherScrape.py
    Quick attempt at opening to the local weather page via Selenium module

--- weatherScrape.py
+++ weatherScrape.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import bs4, requests
+from selenium import webdriver
 
 def getBentonvilleWeather(weatherUrl):
     res = requests.get(weatherUrl)
@@ -21,7 +22,15 @@ def getBentonvilleTemp(tempUrl):
     return elemsTwo[0].text.strip()
 
 
+
 weather = getBentonvilleWeather('https://forecast.weather.gov/MapClick.php?lat=36.368930000000034&lon=-94.21025999999995')
 temp = getBentonvilleTemp('https://forecast.weather.gov/MapClick.php?lat=36.368930000000034&lon=-94.21025999999995')
 
+browser = webdriver.Chrome()
+browser.get('https://forecast.weather.gov/MapClick.php?lat=36.368930000000034&lon=-94.21025999999995')
+elem = browser.find_element_by_css_selector('#localWFO')
+elem.click()
+
+
 print('The current weather in Bentonville is ' + weather + ' and ' + temp)
+
